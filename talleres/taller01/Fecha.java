7de4d41127ed46c740a5095953c26892b059fbe5
mavelasqur <73123584+mavelasqur@users.noreply.github.com>
2021-01-29 11:24:36 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -1,20 +1,12 @@
-
-/**
- * Write a description of class Fecha here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Fecha
+public class Fecha 
 {
     private final int dia;
     private final int mes;
     private final int año;
-    
-    public Fecha(int dia, int mes, int año){
-        this.dia = dia;
-        this.año = año;
-        this.mes = mes;
+    public Fecha(int d, int m, int a){
+        this.dia = d;
+        this.mes = m;
+        this.año = a;
     }
     public int dia(){
         return dia;
@@ -25,16 +17,15 @@ public class Fecha
     public int año(){
         return año;
     }
-    /** -1 Menor, 0 Igual, 1 Mayor*/
-    public int comparar(Fecha otro){
-        if(this.año<otro.año){
+    public int comparar(Fecha otra){
+        if (this.dia < otra.dia || this.mes < otra.mes || this.año < otra.año){
             return -1;
-    }else if(this.mes<otro.mes){
-        return -1;
-    }else if(this.dia<otro.dia){
-        return -1;
-    }else{return 1;}
-}
+        } else if (this.dia == otra.dia || this.mes == otra.mes || this.año == otra.año){
+            return 0;
+        } else {
+            return 1;
+        } 
+    }
     public String toString(){
         return (this.dia)+"/"+(this.mes)+"/"+(this.año);
     }
